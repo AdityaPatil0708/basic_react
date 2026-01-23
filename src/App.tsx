380b06edee90ea752eae0cb3ca4f92be82e0aff7
Aditya Patil <adityas_mac@Adityas-MacBook-Air.local>
2026-01-23 18:58:12 +0530

    useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,37 @@
+import { useEffect, useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  function increasecount() {
+    setCount(count + 1);
+  }
+
+  useEffect(function () {
+    let clock = setInterval(function () {
+
+      setCount(count => count + 1);
+    }, 1000);
+    
+    return function(){
+      clearInterval(clock)
+    }
+  }, []);
+
   return (
-    <div className="text-center text-2xl">
-      Hello World!
+    <div className="text-center text-2xl mt-10">
+      <h3>{count}</h3>
+      <div className="flex gap-2 justify-center">
+        <button
+          onClick={increasecount}
+          className="hover:cursor-pointer border p-1"
+        >
+          increase count
+        </button>
+        <br />
+      </div>
     </div>
-  )
+  );
 }
 
 export default App
