4db02ad3b3116018e90a95259337edb850ff7f53
Aditya Patil <adityas_mac@Adityas-MacBook-Air.local>
2026-02-02 18:08:45 +0530

    changed props

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import Card from "./components/Card";
 export default function App() {
   return(
     <div className="flex flex-row gap-5 justify-center">
-      <Card imageSrc="./vite.svg" testimonial="Thank you, Classbot, for providing and affordable application with excellent student data management and fee management features for our coaching institute!" name="Aditya Patil" role="Web Developer"/>
-      <Card imageSrc="./vite.svg" testimonial="Thank you, Classbot, for providing and affordable application with excellent student data management and fee management features for our coaching institute!" name="Aditya Patil" role="Data Analyst"/>
+      <Card imageSrc="./vite.svg" text="Thank you, Classbot, for providing and affordable application with excellent student data management and fee management features for our coaching institute!" name="Aditya Patil" role="Web Developer"/>
+      <Card imageSrc="./vite.svg" text="Thank you, Classbot, for providing and affordable application with excellent student data management and fee management features for our coaching institute!" name="Aditya Patil" role="Data Analyst"/>
     </div>
   )
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,20 +1,20 @@
 interface CardProps {
     imageSrc: string;
     imageAlt?: string;
-    testimonial: string;
+    text: string;
     name: string;
     role: string;
 }
 
-export default function Card({ imageSrc, imageAlt = "Profile", testimonial, name, role }: CardProps) {
+export default function Card({ imageSrc, text, name, role }: CardProps) {
     return (
         <div className="flex justify-center items-center h-screen">
             <div className="border flex flex-row gap-5 p-5 rounded-md w-120">
                 <div>
-                    <img src={imageSrc} alt={imageAlt} className="rounded-full h-30 w-120" />
+                    <img src={imageSrc} className="rounded-full h-30 w-120" />
                 </div>
                 <div className="flex flex-col">
-                    <p className="mb-1">{testimonial}</p>
+                    <p className="mb-1">{text}</p>
                     <div className="text-right">
                         {name} <br />
                         <span className="font-bold">{role}</span>
